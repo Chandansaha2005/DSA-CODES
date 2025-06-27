345c975b38d6fee0cf3b352c53e7d025ce7d41c2
Chandan Saha <chandansaha1945@gmail.com>
2025-06-27 22:27:06 +0530

    bfs dfs code

--- graph/BFS_DFS.c
+++ graph/BFS_DFS.c
@@ -12,29 +12,37 @@ char stack[MAX];
 int top = -1;
 
 // Helper function to add edge in the graph
-void addEdge(char vertices[], int size, char u, char v) {
+void addEdge(char vertices[], int size, char u, char v)
+{
     int uIndex = -1, vIndex = -1;
-    for (int i = 0; i < size; i++) {
-        if (vertices[i] == u) uIndex = i;
-        if (vertices[i] == v) vIndex = i;
+    for (int i = 0; i < size; i++)
+    {
+        if (vertices[i] == u)
+            uIndex = i;
+        if (vertices[i] == v)
+            vIndex = i;
     }
     adjMatrix[uIndex][vIndex] = 1;
     adjMatrix[vIndex][uIndex] = 1;
 }
 
 // BFS function
-void BFS(char vertices[], int size, char start) {
+void BFS(char vertices[], int size, char start)
+{
     printf("BFS Traversal: ");
     int startIndex = -1;
-    
-    for (int i = 0; i < size; i++) {
-        if (vertices[i] == start) {
+
+    for (int i = 0; i < size; i++)
+    {
+        if (vertices[i] == start)
+        {
             startIndex = i;
             break;
         }
     }
 
-    if (startIndex == -1) {
+    if (startIndex == -1)
+    {
         printf("Start node not found!\n");
         return;
     }
@@ -44,20 +52,25 @@ void BFS(char vertices[], int size, char start) {
     queue[rear] = start;
     visited[startIndex] = 1;
 
-    while (front <= rear) {
+    while (front <= rear)
+    {
         char node = queue[front++];
         printf("%c ", node);
 
         int nodeIndex = -1;
-        for (int i = 0; i < size; i++) {
-            if (vertices[i] == node) {
+        for (int i = 0; i < size; i++)
+        {
+            if (vertices[i] == node)
+            {
                 nodeIndex = i;
                 break;
             }
         }
 
-        for (int i = 0; i < size; i++) {
-            if (adjMatrix[nodeIndex][i] == 1 && !visited[i]) {
+        for (int i = 0; i < size; i++)
+        {
+            if (adjMatrix[nodeIndex][i] == 1 && !visited[i])
+            {
                 queue[++rear] = vertices[i];
                 visited[i] = 1;
             }
@@ -67,18 +80,22 @@ void BFS(char vertices[], int size, char start) {
 }
 
 // DFS function
-void DFS(char vertices[], int size, char start) {
+void DFS(char vertices[], int size, char start)
+{
     printf("DFS Traversal: ");
     int startIndex = -1;
 
-    for (int i = 0; i < size; i++) {
-        if (vertices[i] == start) {
+    for (int i = 0; i < size; i++)
+    {
+        if (vertices[i] == start)
+        {
             startIndex = i;
             break;
         }
     }
 
-    if (startIndex == -1) {
+    if (startIndex == -1)
+    {
         printf("Start node not found!\n");
         return;
     }
@@ -87,20 +104,25 @@ void DFS(char vertices[], int size, char start) {
     stack[top] = start;
     visited[startIndex] = 1;
 
-    while (top >= 0) {
+    while (top >= 0)
+    {
         char node = stack[top--];
         printf("%c ", node);
 
         int nodeIndex = -1;
-        for (int i = 0; i < size; i++) {
-            if (vertices[i] == node) {
+        for (int i = 0; i < size; i++)
+        {
+            if (vertices[i] == node)
+            {
                 nodeIndex = i;
                 break;
             }
         }
 
-        for (int i = 0; i < size; i++) {
-            if (adjMatrix[nodeIndex][i] == 1 && !visited[i]) {
+        for (int i = 0; i < size; i++)
+        {
+            if (adjMatrix[nodeIndex][i] == 1 && !visited[i])
+            {
                 stack[++top] = vertices[i];
                 visited[i] = 1;
             }
@@ -110,34 +132,39 @@ void DFS(char vertices[], int size, char start) {
 }
 
 // Reset visited array
-void resetVisited(int size) {
-    for (int i = 0; i < size; i++) {
+void resetVisited(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         visited[i] = 0;
     }
 }
 
-int main() {
+int main()
+{
     int n, e;
     char u, v, start;
-    
+
     // Input number of nodes
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
     char vertices[n];
-    
+
     // Input the vertices
     printf("Enter the vertices (characters): ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf(" %c", &vertices[i]);
     }
-    
+
     // Input number of edges
     printf("Enter number of edges: ");
     scanf("%d", &e);
-    
+
     // Input edges
-    for (int i = 0; i < e; i++) {
+    for (int i = 0; i < e; i++)
+    {
         printf("Enter edge (u v): ");
         scanf(" %c %c", &u, &v);
         addEdge(vertices, n, u, v);
