daba556af78ef33bd9ceb3e8d6aa008561cf2ad1
Chandan Saha <chandansaha1945@gmail.com>
2025-08-21 00:37:00 +0530

    create list

--- collage Assignments/Linked List/Single_LinkedList.c
+++ collage Assignments/Linked List/Single_LinkedList.c
@@ -7,12 +7,25 @@ struct Node
 };
 struct Node *head;
 int count = 0, tempo;
-void createlist(int n){
-     struct Node *temp , *newNode;
-    int x,i;
-    head =(struct Node *)malloc(sizeof(struct Node));
-    
+void createlist(int n)
+{
+   struct Node * newNode , *temp;
+   int x,i;
+   head = NULL
 }
-void main(){
+void display()
+{
+    struct Node *p;
+    p = head;
+    printf("\n------- :Linked List: -------\n");
+    while (p != NULL)
+    {
+        printf("Value = %d Address = %u\n", p->data, p);
+        p = p->next;
+    }
+}
+void main()
+{
     createlist(2);
+    display();
 }
