c8245bbe15744f63a410cabcae6460e9089a288e
Chandan Saha <chandansaha1945@gmail.com>
2025-08-20 23:48:12 +0530

    linkedlist practice

---
+++ collage Assignments/Linked List/Single_LinkedList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *head;
+int count = 0, tempo;
+void createlist(int n){
+     struct Node *temp , *newNode;
+    int x,i;
+    head =(struct Node *)malloc(sizeof(struct Node));
+    
+}
+void main(){
+    createlist(2);
+}
