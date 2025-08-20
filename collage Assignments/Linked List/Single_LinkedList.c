4ee5ad1fc8cbdaa7e16dfc3f2cd72a4704ffcf44
Chandan Saha <chandansaha1945@gmail.com>
2025-08-21 00:51:16 +0530

    create list function done

--- collage Assignments/Linked List/Single_LinkedList.c
+++ collage Assignments/Linked List/Single_LinkedList.c
@@ -9,9 +9,30 @@ struct Node *head;
 int count = 0, tempo;
 void createlist(int n)
 {
-   struct Node * newNode , *temp;
-   int x,i;
-   head = NULL
+    struct Node *newNode, *temp;
+    int x, i;
+    head = NULL;
+    count = 0;
+    for (i = 1; i <= n; i++)
+    {
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        printf("Enter the number = ");
+        scanf("%d", &x);
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            temp = head;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+        count++;
+    }
 }
 void display()
 {
