4b944ec0d8eb8ae81ae00456642172e83ce2b298
Chandan Saha <chandansaha1945@gmail.com>
2025-08-28 00:27:09 +0530

    done

--- collage Assignments/Linked List/prac.c
+++ collage Assignments/Linked List/prac.c
@@ -31,7 +31,56 @@ void createNode(int n)
         count++;
     }
 }
+void insertAtBeg()
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    printf("\nEnter the Element = ");
+    scanf("%d", &newNode->data);
+    newNode->next = head;
+    head = newNode;
+}
+void insertAtEnd()
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node)), *temp;
+    printf("\nEnter the Element = ");
+    scanf("%d", &newNode->data);
+    newNode->next = NULL;
+    if (head == NULL)
+        head = newNode;
+    else
+    {
+        temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void display()
+{
+    struct Node *p;
+    p = head;
+    if (p == NULL)
+        printf("\nEmpty Linked List\n");
+    else
+    {
+        while (p != NULL)
+        {
+            printf("%d --> ", p->data);
+            p = p->next;
+            if (p == NULL)
+                printf("NULL\n");
+        }
+    }
+}
 void main()
 {
+    display();
     createNode(5);
+    display();
+    insertAtBeg();
+    display();
+    insertAtEnd();
+    display();
 }
